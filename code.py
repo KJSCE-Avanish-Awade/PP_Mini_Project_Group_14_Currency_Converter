7d5aa042ff29d846413cae8edc01965dc47f86da
Ranak Bhambra <106725835+RanakBhambra@users.noreply.github.com>
2022-07-04 10:56:02 +0530

    GUI initiated
    GUI initiated variables remaining.

--- code.py
+++ code.py
@@ -8,3 +8,13 @@ from tkinter import *
 import tkinter.messagebox
 
 #GUI Starts from here
+gui = tk.Tk()
+
+gui.title("Currency Converter")
+
+Tops = Frame(gui, bg = '#e6e5e5', pady=2, width=1850, height=100, relief="ridge")
+Tops.grid(row=0, column=0)
+
+headlabel = tk.Label(Tops, font=('lato black', 19, 'bold'), text='		Currency Converter',
+					bg='#e6e5e5', fg='black')
+headlabel.grid(row=1, column=0, sticky=W
