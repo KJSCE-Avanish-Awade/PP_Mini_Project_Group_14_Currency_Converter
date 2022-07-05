57e9593c19ee6e652dd18dabb8975127c82be231
KJSCE-Avanish-Awade <102681538+KJSCE-Avanish-Awade@users.noreply.github.com>
2022-07-05 23:36:42 +0530

    Added the function for Real time conversion

--- code.py
+++ code.py
@@ -26,3 +26,19 @@ variable1.set("Currency")
 variable2.set("Currency")
 
 #Function To For Real Time Currency Conversion starts here
+
+def RealTimeCurrencyConversion():
+	from forex_python.converter import CurrencyRates
+	c = CurrencyRates()
+
+	from_currency = variable1.get()
+	to_currency = variable2.get()
+
+	if (Amount1_field.get() == ""):
+		tkinter.messagebox.showinfo("Error !!", "Amount Not Entered.\n Please a valid amount.")
+
+	elif (from_currency == "currency" or to_currency == "currency"):
+		tkinter.messagebox.showinfo("Error !!",
+					"Currency Not Selected.\n Please select FROM and TO Currency form menu.")
+
+#clearing all the data entered by the user
